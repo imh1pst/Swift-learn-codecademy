476036bf1ee63c48d0064144810e77174a286ae9
Soyhipst <hello.codehipster@outlook.com>
2022-09-18 16:57:22 -0500

    first commit

---
+++ blockletters.swift
@@ -0,0 +1,18 @@
+//my name
+
+print("      AA")
+print("     AAAA")
+print("    AA  AA ")
+print("   AA    AA")
+print("  AAAAAAAAAA")
+print(" AA        AA")
+print("AA          AA")
+
+print("mmmmm      mmmmm")
+print("mm  mm   mm   mm")
+print("mm   mmmmm    mm")
+print("mm            mm")
+print("mm            mm")
+print("mm            mm")
+print("mm            mm")
+
---
+++ learn-swift/piggybank.swift
@@ -0,0 +1,14 @@
+var pesos = 1100
+var reais = 5000
+var soles = 300
+var totalUSD = 0
+
+print(totalUSD)
+
+// 1 Peso equals 0.00029 USD
+// 1 Real equals 0.24 USD
+// 1 Sol equals 0.29 USD
+
+total = 0.00029 * pesos + 0.24 * reais + 0.29 * soles
+
+print("totalUSD = $\(total)")
